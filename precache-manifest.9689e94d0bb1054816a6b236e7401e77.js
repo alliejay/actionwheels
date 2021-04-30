0d1109fd166d1e5d71370b693a2a42e4fe5f4a38
Allie A <alliejarsenault@gmail.com>
2021-04-29 20:40:45 -0400

    Updates

---
+++ precache-manifest.9689e94d0bb1054816a6b236e7401e77.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b62e316dfd8ac2bd1135bc3a210b4e",
+    "url": "/review/index.html"
+  },
+  {
+    "revision": "c395d1d6f8bf6d3ea276",
+    "url": "/review/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "0add959a348c70aa76fd",
+    "url": "/review/static/css/main.0be1300d.chunk.css"
+  },
+  {
+    "revision": "c395d1d6f8bf6d3ea276",
+    "url": "/review/static/js/2.1a6e71b6.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/review/static/js/2.1a6e71b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0add959a348c70aa76fd",
+    "url": "/review/static/js/main.25eed90b.chunk.js"
+  },
+  {
+    "revision": "fa947e0823e616f4297e",
+    "url": "/review/static/js/runtime-main.2521d675.js"
+  },
+  {
+    "revision": "c7494dd617eae43275c7e160a9c45c15",
+    "url": "/review/static/media/action_wheels.c7494dd6.png"
+  },
+  {
+    "revision": "a94d5bf9182703619546d9d626f4a486",
+    "url": "/review/static/media/actionwheels.a94d5bf9.jpeg"
+  },
+  {
+    "revision": "680db5db6b860f95c55b34be1b7f1efb",
+    "url": "/review/static/media/batman.680db5db.png"
+  },
+  {
+    "revision": "8085d325dbed129c776cfbcf30f92710",
+    "url": "/review/static/media/batman12in.8085d325.jpeg"
+  },
+  {
+    "revision": "a99dc0ad189c17178d6862297e4d5d6b",
+    "url": "/review/static/media/batman16in.a99dc0ad.jpeg"
+  },
+  {
+    "revision": "e20efb319a06f3b34c6ec544876753a8",
+    "url": "/review/static/media/batmobile.e20efb31.jpeg"
+  },
+  {
+    "revision": "3ec034e99fc372ac0cb74790be614268",
+    "url": "/review/static/media/batmobile6vt.3ec034e9.jpeg"
+  },
+  {
+    "revision": "fbcef838dc4ece81421f48a9359fa845",
+    "url": "/review/static/media/columbia2.fbcef838.png"
+  },
+  {
+    "revision": "6eb11cf1430362d18bc00eb01ed7458e",
+    "url": "/review/static/media/columbiaHybrid700.6eb11cf1.png"
+  },
+  {
+    "revision": "8b462877d243bbfab76fdd63f50aa535",
+    "url": "/review/static/media/columbiaTeton20.8b462877.png"
+  },
+  {
+    "revision": "7db6f7fd322b3ba587fda5113dbb3028",
+    "url": "/review/static/media/disney.7db6f7fd.jpg"
+  },
+  {
+    "revision": "920a813cae062b71d92c5c3e8b00759f",
+    "url": "/review/static/media/disneyFrozen.920a813c.jpeg"
+  },
+  {
+    "revision": "694673f498088d164c8acd53a2eb92ca",
+    "url": "/review/static/media/disneyFrozen6.694673f4.jpeg"
+  },
+  {
+    "revision": "1166ee1c0cddb6f69e9e528df2670d07",
+    "url": "/review/static/media/disneyFrozenScooter.1166ee1c.jpeg"
+  },
+  {
+    "revision": "ee0f4fadb0df640322a2a0dd62c90475",
+    "url": "/review/static/media/disneyPrincess.ee0f4fad.jpeg"
+  },
+  {
+    "revision": "80f3062a52ee7e3b6ab7f52ab70169e3",
+    "url": "/review/static/media/disneySofia.80f3062a.jpeg"
+  },
+  {
+    "revision": "8c8bb636ba4029fe1308028f74a73cfd",
+    "url": "/review/static/media/disney_banner.8c8bb636.jpg"
+  },
+  {
+    "revision": "c4a294281c5f34f5d5493b02007f3bde",
+    "url": "/review/static/media/frozensleigh.c4a29428.jpeg"
+  },
+  {
+    "revision": "d381196dd695978f15f1855d1fca46c5",
+    "url": "/review/static/media/honda-6vt.d381196d.jpeg"
+  },
+  {
+    "revision": "439dd83cdce442035c98634b038dcf67",
+    "url": "/review/static/media/honda.439dd83c.jpg"
+  },
+  {
+    "revision": "a0c585bc3117f5a63580d6a9f8d11efa",
+    "url": "/review/static/media/honda12vt.a0c585bc.jpeg"
+  },
+  {
+    "revision": "046cefaf76750ef4792d46df2a9e5940",
+    "url": "/review/static/media/honda6vtScooter.046cefaf.jpeg"
+  },
+  {
+    "revision": "e82b07373def8f5871fde8cde4ad319b",
+    "url": "/review/static/media/hondaBlack12.e82b0737.jpeg"
+  },
+  {
+    "revision": "d381196dd695978f15f1855d1fca46c5",
+    "url": "/review/static/media/hondaBlack6vt.d381196d.jpeg"
+  },
+  {
+    "revision": "3dab9a3abad9eaef5a24f4f0e60a2e2f",
+    "url": "/review/static/media/hondaGrizzly12vt.3dab9a3a.jpeg"
+  },
+  {
+    "revision": "5a53b383cd0b70504e97dbf01b41deba",
+    "url": "/review/static/media/logo.5a53b383.png"
+  },
+  {
+    "revision": "d67206d4c841f7bf8b36e73603de9dce",
+    "url": "/review/static/media/minnieMouse.d67206d4.jpeg"
+  },
+  {
+    "revision": "06a534af3058990e14d9fed2c51b0bba",
+    "url": "/review/static/media/seascooter.06a534af.jpeg"
+  },
+  {
+    "revision": "aaae3a78e565c00997b2a388a9f75308",
+    "url": "/review/static/media/seascooter.aaae3a78.jpg"
+  },
+  {
+    "revision": "4f662a721f3c8ee9815b0eee2b3c7832",
+    "url": "/review/static/media/talon_banner.4f662a72.jpg"
+  },
+  {
+    "revision": "b5069862d8f185ecb12bcd09d5adb8c9",
+    "url": "/review/static/media/thunderTank.b5069862.jpeg"
+  },
+  {
+    "revision": "4a2a6cc223a820efb2671765d587fd68",
+    "url": "/review/static/media/warnerbros.4a2a6cc2.png"
+  },
+  {
+    "revision": "a50e98fab0d2ecd7566a6ece5915e673",
+    "url": "/review/static/media/wonderWoman16in.a50e98fa.jpeg"
+  },
+  {
+    "revision": "7ac0534c150a51201a761a99030b6f51",
+    "url": "/review/static/media/yamaha.7ac0534c.jpg"
+  },
+  {
+    "revision": "558004fffc5d80391d9a87f154caf5cc",
+    "url": "/review/static/media/yamaha12in.558004ff.jpeg"
+  },
+  {
+    "revision": "75232b4e314fe7729c82262c67e9486e",
+    "url": "/review/static/media/yamaha12vt.75232b4e.jpeg"
+  },
+  {
+    "revision": "9a79d9cafa309330e10a2aa1e3a5c8e3",
+    "url": "/review/static/media/yamaha12vtXYZ.9a79d9ca.jpeg"
+  },
+  {
+    "revision": "ec1e47c1f2af6591a594968947872551",
+    "url": "/review/static/media/yamaha16in.ec1e47c1.jpeg"
+  },
+  {
+    "revision": "d973c241a4b701d0d0bf97de7f8b79f3",
+    "url": "/review/static/media/yamahaViking.d973c241.jpeg"
+  },
+  {
+    "revision": "d06d4cb4187e7c97c5e78858ca2e96af",
+    "url": "/review/static/media/youtube-icon.d06d4cb4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22b27f1d2470159a42580a3c236e3df0.js"
+  "/review/precache-manifest.9689e94d0bb1054816a6b236e7401e77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/review/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
